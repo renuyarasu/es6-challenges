a81bf821ad833149fa298dabd818933c200e2f0a
Renuka Prasad <renu.yarasu@gmail.com>
2023-01-11 09:58:06 +0530

    Let

--- script.js
+++ script.js
@@ -3,18 +3,15 @@
 "use strict"
 console.clear();
 
-/* CHALLANGE 2 - Const
-Answer following Qustions:
-  1. Why no error is generated after the line 14?
-  2. Why after the line 19 TypeError is generated?
+/* CHALLENGE 3 - Let
 
-Change one line of code so, that error will go away.
-Don't change lines 14, 19.
+Change code to match output.
 */
 
+var i = 10;
 
-const newArr = [1, 2, 3];
-newArr.push(4);
-console.log(newArr);
-newArr = [1, 2, 3, 4, 5];
-console.log(newArr);
+for (let i = 0; i <= 5; i++) {
+    console.log(i);
+}
+
+console.log(i);
