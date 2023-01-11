18c1f1a6a0a24087602dcfe620754c8cf6b969c9
Renuka Prasad <renu.yarasu@gmail.com>
2023-01-11 09:14:37 +0530

    Sum Numbers

--- script.js
+++ script.js
@@ -3,4 +3,19 @@
 "use strict"
 console.clear();
 
+/*
+Create a function sum() that will sum all arguments passed to it. Quantity of the arguments is unknown.
+Use console.log() at the end of the sum() function to print result. Use ES6 whenever possible. */
+
+function sum(...args) {
+    // console.log(args);
+    let total;
+    total = args.reduce((acc, ele) => acc + ele);
+    console.log(total);
+}
+
+
+sum(1, 2, 3)
+sum(1, 2, 3, 4, 5, 6)
+sum(1, 2, 3, 4, 5, 6, 7, 8, 9)
 
