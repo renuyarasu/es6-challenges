e5b5b6a86931050b8fcd59900120ed255f5c3793
Renuka Prasad <renu.yarasu@gmail.com>
2023-01-12 21:43:08 +0530

    Template Literals

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
 // JavaScript and ES6 Challenges - Do you know JavaScript - Created by Bogdan Stashchuk
 "use strict"
 console.clear();
-/* CHALLENGE 12 - Copy Array
-Create copy of the a array.*/
+/* CHALLENGE 13 - Template Literals
 
-var a = [1, 2, 3];
+Create carInfo() function that
+will return info about each car.
 
-var b;
+Car is considered cheap if it's price is <= 20000
+Car is considered expensive it it's price is > 20000 */
 
-b = a;
-b = [...a];
-b = a.slice();
-b.push('Veda')
+var cars = [
+    { brand: 'Honda', price: '10L' },
+    { brand: 'Rolls-Royce', price: '10C' }
+]
 
+function carInfo(car) {
+    return `Price of my new ${car.brand} is ${car.price}  and it is cheap car.`
+}
 
-
-console.log(a);
-console.log(b);
+cars.forEach(car => console.log(carInfo(car)));
