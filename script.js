09945a4fb8ee3c57f466f469d68926fff433fba2
Renuka Prasad <renu.yarasu@gmail.com>
2023-01-30 16:30:05 +0530

    Template Literals - Challenge 13

--- script.js
+++ script.js
@@ -10,12 +10,17 @@ Car is considered cheap if it's price is <= 20000
 Car is considered expensive it it's price is > 20000 */
 
 var cars = [
-    { brand: 'Honda', price: '10L' },
-    { brand: 'Rolls-Royce', price: '10C' }
-]
+    { brand: 'Honda', price: 13000 },
+    { brand: 'Rolls-Royce', price: 120000 }
+];
 
 function carInfo(car) {
-    return `Price of my new ${car.brand} is ${car.price}  and it is cheap car.`
+    let s;
+    s = (car.price <= 20000) ? 'cheep' : 'expensive'
+    /* if (car.price <= 20000) s = 'cheep'
+    else s = 'expensive' */
+
+    return `Price of my new ${car.brand} is ${car.price}$ and it is ${(car.price <= 20000) ? 'cheep' : 'expensive'} car.`
 }
 
-cars.forEach(car => console.log(carInfo(car)));
+cars.forEach(car => console.log(carInfo(car))); 
